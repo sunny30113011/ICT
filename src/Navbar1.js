180b0fc0a76022dbb77fd1658a58a763ba329493
sunny30113011 <sjtech3011@gmail.com>
2025-03-05 01:09:40 +0530

    Update Navbar1.js

--- src/Navbar1.js
+++ src/Navbar1.js
@@ -10,7 +10,7 @@ function Navbar1()
     return(
         <>
        
-    <nav class="navbar navbar-expand-lg bg-info">
+    <nav class="navbar navbar-expand-lg bg-dark">
   <div class="container-fluid">
   <a class="navbar-brand" href="#" >
      
